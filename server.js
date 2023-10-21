9533743d7438a831b1100b2eaf12d598bd071ebf
joseph-winner <muhanguzijoseph2021@gmail.com>
2023-10-21 21:07:23 +0300

    changes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.set("view engine", "ejs");
 app.use("/public", express.static("public"));
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3005;
 //rendering homepage
 app.listen(port, () => {
   console.log("app listening on " + port);
